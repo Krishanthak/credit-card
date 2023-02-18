1da2771c30e9d44db7c504ceaac86be191f9b80d
DESKTOP-TIVL2VM\Krishantha <Krishantha9331@gmail.com>
2023-02-18 16:29:46 +0530

    test

--- Application Tier/Business Layer/CreditCard.Core.Tests/UnitTest1.cs
+++ Application Tier/Business Layer/CreditCard.Core.Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using CreditCard.Common.Model.CreditCard;
+using CreditCard.Core.CreditCard;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CreditCard.Core.Tests
@@ -9,6 +11,31 @@ namespace CreditCard.Core.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            //Expected
+
+            Validation expected_result1 = new Validation()
+            {
+                CardNumber = "4111111111111111",
+                CardType = "Visa",
+                Validity = "Valid"
+            };
+
+            Validation expected_result2 = new Validation()
+            {
+                CardNumber = "4111111111111",
+                CardType = "Visa",
+                Validity = "Invalid"
+            };
+
+            //Actual
+            ValidationService validationService = new ValidationService();
+            Validation actual_result1 = validationService.CreditCardValidation("4111111111111111");
+            Validation actual_result2 = validationService.CreditCardValidation("4111111111111");
+
+            Assert.AreEqual(expected_result1.CardType, actual_result1.CardType);
+            Assert.AreEqual(expected_result1.Validity, actual_result1.Validity);
+            Assert.AreEqual(expected_result2.CardType, actual_result2.CardType);
+            Assert.AreEqual(expected_result2.Validity, actual_result2.Validity);
         }
     }
 }
