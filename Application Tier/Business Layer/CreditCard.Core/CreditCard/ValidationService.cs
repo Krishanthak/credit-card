28652295005cba20b684297d245884592b626fb0
DESKTOP-TIVL2VM\Krishantha <Krishantha9331@gmail.com>
2023-02-18 15:55:18 +0530

    remove unwanted

--- Application Tier/Business Layer/CreditCard.Core/CreditCard/ValidationService.cs
+++ Application Tier/Business Layer/CreditCard.Core/CreditCard/ValidationService.cs
@@ -32,7 +32,7 @@ namespace CreditCard.Core.CreditCard
             return returnData;
         }
 
-        private string NumberValidation(string creditCard)
+        private static string NumberValidation(string creditCard)
         {
             try
             {
@@ -59,60 +59,11 @@ namespace CreditCard.Core.CreditCard
                     }
                 }
                 return sum % 10 == 0 ? "Valid" : "Invalid";
-
-
-                //ArrayList CheckNumbers = new ArrayList();
-
-                //creditCard = creditCard.Replace(" ", "");
-                //int CardLength = creditCard.Length;
-
-                //for (int i = CardLength - 2; i >= 0; i = i - 2)
-                //{
-                //    CheckNumbers.Add(Int32.Parse(creditCard[i].ToString()) * 2);
-                //}
-                //int CheckSum = 0;
-                //for (int iCount = 0; iCount <= CheckNumbers.Count - 1; iCount++)
-                //{
-                //    int _count = 0;
-                //    if ((int)CheckNumbers[iCount] > 9)
-                //    {
-                //        int _numLength = ((int)CheckNumbers[iCount]).ToString().Length;
-                //        for (int x = 0; x < _numLength; x++)
-                //        {
-                //            _count = _count + Int32.Parse(
-                //                  ((int)CheckNumbers[iCount]).ToString()[x].ToString());
-                //        }
-                //    }
-                //    else
-                //    {
-                //        _count = (int)CheckNumbers[iCount];
-                //    }
-                //    CheckSum = CheckSum + _count;
-                //}
-                //int OriginalSum = 0;
-                //for (int y = CardLength - 1; y >= 0; y = y - 2)
-                //{
-                //    OriginalSum = OriginalSum + Int32.Parse(creditCard[y].ToString());
-                //}
-
-                //return (((OriginalSum + CheckSum) % 10) == 0);
             }
             catch (Exception ex)
             {
                 throw ex;
             }
         }
-
-        //private static string GenerateCardType(string creditCard)
-        //{
-        //    if ((Regex.IsMatch(creditCard, "^(34|37)"))) return CardType.Amex;
-        //    else if ((Regex.IsMatch(creditCard, "^(51|52|53|54|55)"))) return "MasterCard";
-        //    else if ((Regex.IsMatch(creditCard, "^(4)"))) return "Visa";
-        //    else if ((Regex.IsMatch(creditCard, "^(6011)"))) return "Discover";
-        //    else return "Unknown";
-        //}
-
-
-
     }
 }
